9c5892ab571883bbc45ef0f6100a1e6b8ff2d1bd
Enzo Cesar <enzoconsulo@gmail.com>
2025-07-23 11:17:35 -0300

    Added new package DTO, create UserDTO filtering the password(added now to illustrate DTO function) from User to front

--- src/main/java/com/enzoccs/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/enzoccs/springbootmongodb/config/Instantiation.java
@@ -20,9 +20,9 @@ public class Instantiation implements CommandLineRunner{
 
 		userRepository.deleteAll();
 		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User maria = new User(null, "Maria Brown", "maria@gmail.com","1234");
+		User alex = new User(null, "Alex Green", "alex@gmail.com","12345");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com","4321");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 	}
